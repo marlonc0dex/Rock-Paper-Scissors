cf1b405d87d02d0e9230c50bdea94877fd8ae111
marlonc0dex <sssasuke40@gmail.com>
2022-11-28 20:07:31 -0300

    Dividindo a janela

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ co5 = "#e85151"   # red / vermelha
 
 fundo = "#3b3b3b"
 
+# configurando a janela
 janela = Tk()
 janela.title("")
 janela.geometry('260x280')
 janela.configure(bg=fundo)
 
-janela.mainloop()
+# dividindo a janela
 
+frame_cima = Frame(janela, width=260, height=100, bg=co1, relief='raised')
+frame_cima.grid(row=1, column=0, sticky=NW)
+
+
+janela.mainloop()
